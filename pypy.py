4558022bda1ceebf029acc62d99a62cffe2c6eae
NkosiV <Snuttie12@gmail.com>
2024-10-01 16:10:50 +0200

    first commit

---
+++ pypy.py
@@ -0,0 +1,21 @@
+# fruits = ["Apple", "Strawberry", "Watermelon"]
+# adjectives = ["Red", "Delicious", "Sweet"]
+
+# for y in range(3):
+#     global lim
+#     lim = []
+#     lim = adjectives[y] + " " + fruits[y]
+#     print(y)
+#     print(lim)
+# print(lim)
+
+fruits = ["Apple", "Strawberry", "Watermelon"]
+adjectives = ["Red", "Delicious", "Sweet"]
+
+for y in range(3):
+    lim = adjectives[y] + " " + fruits[y]  # Combine adjective and fruit
+    # print(y)  # Print the index
+    print(lim)  # Print the combined string
+
+# Print the last value of lim
+# print(lim)
